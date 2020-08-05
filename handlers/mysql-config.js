137225aa474a42a8f47870d0e4ec3c1b7846b444
Taushiq <taushiq@users.github.com>
2020-08-05 19:10:28 +0530

    Remote sql

--- handlers/mysql-config.js
+++ handlers/mysql-config.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//     host: 'localhost',
+//     port: 3306,
+//     database: 'question_bank',
+//     user: 'root',
+//     password: 'Admin@123'
+// };
+
 module.exports = {
-    host: 'localhost',
+    host: 'remotemysql.com',
     port: 3306,
-    database: 'question_bank',
-    user: 'root',
-    password: 'Admin@123'
+    database: '5PJ8RGg4yy',
+    user: '5PJ8RGg4yy',
+    password: 'Y7VsxHDGyN'
 };
